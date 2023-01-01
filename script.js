93f23140874efb798b103a46eb612ffaef0f3470
Zizhen Luo s3839954 <zizhenluo2328@gmail.com>
2023-01-01 23:11:30 +1100

    fixed unclickable cursor due to pre-loader opacity

--- script.js
+++ script.js
@@ -195,4 +195,5 @@ const loader = document.querySelector(".loader--wrapper");
 
 window.addEventListener("load", function () {
   loader.style.opacity = 0;
+  loader.style.visibility = "hidden";
 });
